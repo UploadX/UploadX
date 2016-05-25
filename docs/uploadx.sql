d4d2137607c4df1325ad5d644d045c84e9977c3d
Matt M <matt@mcflux.net>
2016-05-24 20:09:53 -0400

    Missing some tables!

--- docs/uploadx.sql
+++ docs/uploadx.sql
@@ -13,4 +13,23 @@ CREATE TABLE IF NOT EXISTS `uploadx_files` (
   PRIMARY KEY (`rowid`),
   UNIQUE KEY `file_name` (`file_name`),
   UNIQUE KEY `file_id` (`file_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE IF NOT EXISTS uploadx_logs (
+  rowid INT(9) AUTO_INCREMENT PRIMARY KEY,
+  log_level VARCHAR(25) NOT NULL,
+  log_type VARCHAR(100) NOT NULL,
+  log_message VARCHAR(1024) NOT NULL,
+  log_time VARCHAR(20) NOT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS uploadx_users (
+  rowid INT(9) AUTO_INCREMENT PRIMARY KEY,
+  user_id VARCHAR(20) NOT NULL UNIQUE,
+  access_key VARCHAR(20) NOT NULL,
+  filesize_limit VARCHAR(100) NOT NULL,
+  uploads INT (6) NOT NULL DEFAULT 0,
+  enabled BOOL NOT NULL DEFAULT true
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
